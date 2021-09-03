418edb982eae4ba6413270dbe82ada1fe035aa5f
jonathan garzon <jonatangarzon95@gmail.com>
2021-09-03 13:48:19 -0500

    metodo with para hacer todas las consulta

--- app/Http/Controllers/users/UserController.php
+++ app/Http/Controllers/users/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::with(['tags', 'roles', 'note'])->get();
         return view('users.index', compact('users'));
     }
 
